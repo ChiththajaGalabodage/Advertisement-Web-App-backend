b6c745bf94747480838b4c93db0bf4139662f6e7
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-09-22 23:48:20 +0530

    listing model

--- models/listing.js
+++ models/listing.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const listingSchema = mongoose.Schema(
   {
     title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
     slug: { type: String, index: true },
     price: { type: Number, required: true, min: 0 },
     currency: { type: String, default: "LKR" },
